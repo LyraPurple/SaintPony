3d2b7b2d00e66f9c5d794ab0a978a1564d7e03c1
Laugras <lew_terramp@hotmail.fr>
2019-04-02 17:01:36 +0200

    :white_small_square:twig  /discover-symfony/templates/welcome/babou.html.twig et :white_small_square:extends AbstractController

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -3,19 +3,12 @@
 // http://127.0.0.1:8000/babou
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
-/* Ajouter une route en un seul fichier 
-(Méthode aussi efficace que yaml, 
-il y en a pour tout les goûts.) 
-Installer 
-*******************************
-composer require annotations
-*******************************
-*/
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; /* Ajouter ce use pour pouvoir extends AbstractController" */
 
 
 // On va créer une route.
-class WelcomeController
+class WelcomeController extends AbstractController /* Pour hériter de "toutes les méthodes de extends AbstractController" */
 {
     /**
      * @Route("/babou", name="babou")
@@ -24,11 +17,9 @@ class WelcomeController
         $name = 'Boubi';
         // On peut dumper une variable comme var_dump
         dump($name);
-        // Ce sera une route, et chaque route (chaque page) 
-        // doit renvoyer quelque chose. RETURN
-        return
-        // On utilise une classe Response
-        // du nameSpace de Symfony, donc on doit ajouter le use.
-        new Response('<html><body>La rouroute '.$name.'</body></html>');
+        // Rendu de fichier twig pour voir:
+        return $this->render('welcome/babou.html.twig', [
+            'name' => $name
+        ]);
     }
 }
