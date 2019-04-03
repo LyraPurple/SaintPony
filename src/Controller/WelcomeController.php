adbd4d7942f4bd10e1f792a52e3a2e63e54c136c
Laugras <lew_terramp@hotmail.fr>
2019-04-03 10:24:49 +0200

    Route dynamique Etape1: Avoir une route avec un cheming/nom-de-route à donner.

--- src/Controller/WelcomeController.php
+++ src/Controller/WelcomeController.php
@@ -1,6 +1,6 @@
 <?php
 
-// http://127.0.0.1:8000/babou
+// http://127.0.0.1:8000/babou/Boubou
 namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,15 +11,21 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; /* Ajouter ce 
 class WelcomeController extends AbstractController /* Pour hériter de "toutes les méthodes de extends AbstractController" */
 {
     /**
-     * @Route("/babou", name="babou")
+     * @Route("/babou/{nomRoute}", name="babou")
      */
-    public function babou(){
-        $name = 'Boubi';
+    public function babou($nomRoute){  
+            /* public function babou($nomRoute) 
+            rend le paramètre $nomRoute obligatoire.
+            ($nomRoute = 'Paramètre par défault')
+            donne un paramètre à $nomRoute au cas où.*/
+
+
         // On peut dumper une variable comme var_dump
-        dump($name);
+        dump($nomRoute);
         // Rendu de fichier twig pour voir:
+        // \vendor\symfony\framework-bundle\Controller\ControllerTrait.php
         return $this->render('welcome/babou.html.twig', [
-            'name' => $name
+            'name' => ucfirst($nomRoute) /* ucfirst Majuscule la première lettre */
         ]);
     }
 }
