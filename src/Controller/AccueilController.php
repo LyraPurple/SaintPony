021f10d04a12658d5b79b53606d29328ffaf19c5
Laugras <lew_terramp@hotmail.fr>
2019-04-03 11:59:54 +0200

    :white_medium_small_square:Request:white_medium_small_square: : plus de $_GET et $_POST

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -2,6 +2,7 @@
 
 // http://127.0.0.1:8000/babou/*
 namespace App\Controller;
+use Symfony\Component\HttpFoundation\Request;/* Pour récupérer la requête $_GET et $_POST en Symfony */
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; /* Ajouter ce use pour pouvoir extends AbstractController" */
@@ -10,6 +11,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; /* Ajouter ce 
 // On va créer une route.
 class AccueilController extends AbstractController /* Pour hériter de "toutes les méthodes de extends AbstractController" */
 {
+
+    /**
+    * @Route("/", name="naccueil")
+    */
+    public function naccueil(Request $potiteRequete) /* Il n'y a plus de $_GET et $_POST en Symfony */
+    {
+        dump($potiteRequete); /* équivalant à $_GET['parametre-toto']  ou $_POST['parametre-toto']*/
+        // F12 > query > parametres
+        dump($potiteRequete->query->get('parametre-toto', 10)); /* http://127.0.0.1:8000/?parametre-toto=9 */
+        
+        // Est-ce que c'est une requête AJAX
+        dump($potiteRequete->isXmlHttpRequest());
+
+        // Méthode de la requête
+        dump($potiteRequete->isMethod('POST'));
+
+        return $this->render('accueil/naccueil.html.twig');
+    }
+
+
     /**
      * @Route("/babou/{nomRoute}", name="babou", requirements={"^[a-z]{3,8}$"})
      */
@@ -23,13 +44,6 @@ class AccueilController extends AbstractController /* Pour hériter de "toutes l
         ]);
     }
 
-    /**
-    * @Route("/", name="naccueil")
-    */
-    public function naccueil()
-    {
-        return $this->render('accueil/naccueil.html.twig');
-    }
 }
 
 
