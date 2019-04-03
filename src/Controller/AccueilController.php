2e6ead91ed7c99e16479e009d1317c4330ae1c8c
Laugras <lew_terramp@hotmail.fr>
2019-04-03 11:14:39 +0200

    Page d'Accueil :house_with_garden:

--- src/Controller/AccueilController.php
+++ src/Controller/AccueilController.php
@@ -8,29 +8,27 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController; /* Ajouter ce 
 
 
 // On va créer une route.
-class WelcomeController extends AbstractController /* Pour hériter de "toutes les méthodes de extends AbstractController" */
+class AccueilController extends AbstractController /* Pour hériter de "toutes les méthodes de extends AbstractController" */
 {
     /**
      * @Route("/babou/{nomRoute}", name="babou", requirements={"^[a-z]{3,8}$"})
      */
-    // Symfony de base fait: ^$ 
-    // donc {"^[a-z]{3,8}$"} = {"[a-z]{3,8}"}
-    // Dans nos Regex https://regex101.com/
-
     public function babou($nomRoute = 'Boubou'){  
-            /* public function babou($nomRoute) 
-            rend le paramètre $nomRoute obligatoire.
-            ($nomRoute = 'Paramètre par défault')
-            donne un paramètre à $nomRoute au cas où.*/
-
 
         // On peut dumper une variable comme var_dump
         dump($nomRoute);
         // Rendu de fichier twig pour voir:
-        // \vendor\symfony\framework-bundle\Controller\ControllerTrait.php
-        return $this->render('welcome/babou.html.twig', [
+        return $this->render('accueil/babou.html.twig', [
             'name' => ucfirst($nomRoute) /* ucfirst Majuscule la première lettre */
         ]);
+    }
+
+    /**
+    * @Route("/", name="naccueil")
+    */
+    public function naccueil()
+    {
+        return $this->render('accueil/naccueil.html.twig');
     }
 }
 
@@ -43,3 +41,4 @@ $url = $this->generateUrl('product_show', [
     ]);
  *****************
 Celà permet au site de gérer les liens générés selon les langues (par exemple) */
+
